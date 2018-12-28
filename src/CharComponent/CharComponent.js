e06b530d55adc78a289bcdebc222a239ae7742ef
Evan Londono <evanlondono@FLCPLC02WW49TJG5H.local>
2018-12-28 09:42:35 -0500

    added comment explaining my thoughts at that moment

--- src/CharComponent/CharComponent.js
+++ src/CharComponent/CharComponent.js
@@ -7,6 +7,9 @@ const divStyle = {
    margin: '16px',
    border: '1px solid block'
 };
+// I am assuming that I can only use simple terenary code in the JSX which is why this isn't working here I will make this into a function in the app.js
+const CharComponent = ({textAreaContent}) => { return ( <div style = {divStyle}> { this.textAreaContent((textAreaContent , index) => { console.log(textAreaContent[0]) )} }; </div> ) };
 
-const CharComponent = ({textAreaContent}) => { return ( <div style = {divStyle}> { this.textAreaContent((textAreaContent , index) => { console.log(textAreaContent[0]) )} }; </div> ) }
+
+
 export default CharComponent;
